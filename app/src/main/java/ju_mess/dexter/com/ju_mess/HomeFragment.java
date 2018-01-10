865c42a8536ecdfe69fa97e579c0b1f767fd5d50
akshaybengani <akshaybengani@gmail.com>
2018-01-10 11:40:19 +1100

    with menu activity

--- app/src/main/java/ju_mess/dexter/com/ju_mess/HomeFragment.java
+++ app/src/main/java/ju_mess/dexter/com/ju_mess/HomeFragment.java
@@ -1,17 +1,21 @@
 package ju_mess.dexter.com.ju_mess;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+        Button buttonBreakfast,buttonLunch,buttonSnacks,buttonDinner;
 
 
     public HomeFragment() {
@@ -27,9 +31,48 @@ public class HomeFragment extends Fragment {
 
         // TODO This is our OnCreate
 
+        buttonBreakfast = (Button)view.findViewById(R.id.Buttonbreakfast);
+        buttonLunch = (Button)view.findViewById(R.id.ButtonLunch);
+        buttonSnacks = (Button)view.findViewById(R.id.ButtonSnacks);
+        buttonDinner = (Button)view.findViewById(R.id.ButtonDinner);
 
+        buttonBreakfast.setOnClickListener(this);
+        buttonLunch.setOnClickListener(this);
+        buttonSnacks.setOnClickListener(this);
+        buttonDinner.setOnClickListener(this);
 
         return view;
     }
+    @Override
+    public void onClick(View v) {
+        if (v == buttonBreakfast)
+        {
+            Intent intent1 = new Intent(HomeFragment.this,MainActivity.class);
+            intent1.putExtra("Meal Time","Breakfast");
+            startActivity(intent1);
+
+        }
+        if (v == buttonLunch)
+        {
+            Intent intent2 = new Intent(HomeFragment.this,MainActivity.class);
+            intent2.putExtra("Meal Time","Lunch");
+            startActivity(intent2);
+        }
+        if (v == buttonSnacks)
+        {
+            Intent intent3 = new Intent(HomeFragment.this,MainActivity.class);
+            intent3.putExtra("Meal Time","Snacks");
+            startActivity(intent3);
+        }
+        if (v == buttonDinner)
+        {
+            Intent intent4 = new Intent(HomeFragment.this,MainActivity.class);
+            intent4.putExtra("Meal Time","Dinner");
+            startActivity(intent4);
+        }
+
+    }
+
+
 
 }
---
+++ app/src/main/java/ju_mess/dexter/com/ju_mess/Menu.java
@@ -0,0 +1,99 @@
+package ju_mess.dexter.com.ju_mess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class Menu extends AppCompatActivity {
+
+    ListView listView;
+    DatabaseReference databaseReference;
+    ArrayList<String> list = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        String MealTime = getIntent().getExtras().getString("Meal Time");
+
+        listView = (ListView) findViewById(R.id.listview);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,list);
+        listView.setAdapter(adapter);
+
+        // Access Firebase Database
+        databaseReference = FirebaseDatabase.getInstance().getReference(MealTime);
+
+        listDataView();
+
+
+    }
+
+    public void listDataView()
+    {
+        // TODO listDataView Started
+
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                String value = dataSnapshot.getValue(String.class);
+                list.add(value);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                list.remove(value);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        // Function ends -----------
+    }
+
+
+}
